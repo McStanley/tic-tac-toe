8805d112c587eca8adf335d8bdbad0af946a68a2
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2022-06-19 13:24:03 +0200

    Allow players to place their marker on the board

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
+const game = (() => {
+    let activePlayer = null;
+    const getActivePlayer = () => activePlayer;
+    const toggleActivePlayer = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    };
+    const resetActivePlayer = () => {
+        activePlayer = player1;
+    }
+    return {
+        getActivePlayer,
+        toggleActivePlayer,
+        resetActivePlayer,
+    }
+})();
+
 const gameBoard = (() => {
-    let board = ['','X','','','O','','X','X','O'];
+    let board = [];
     const getBoard = () => board;
+    const placeMarker = (e) => {
+        // check if the cell is empty
+        const i = e.target.dataset.index;
+        if (board[i]) return;
+
+        const marker = game.getActivePlayer().getMarker();
+        board[i] = marker;
+        displayController.updateGrid();
+        game.toggleActivePlayer();
+    };
     return {
         getBoard,
+        placeMarker,
     };
 })();
 
 const displayController = (() => {
-    const gridCell = document.querySelectorAll('.grid-cell');
+    const gridCells = document.querySelectorAll('.grid-cell');
+
+    for (const cell of gridCells) {
+        cell.addEventListener('click', gameBoard.placeMarker);
+    }
     const updateGrid = () => {
         const board = gameBoard.getBoard();
         for (const i in board) {
-            gridCell[i].textContent = board[i];
+            gridCells[i].textContent = board[i];
         }
     };
     return {
@@ -19,4 +50,19 @@ const displayController = (() => {
     };
 })();
 
+const Player = (name, marker) => {
+    const getName = () => name;
+    const getMarker = () => marker;
+    return {
+        getName,
+        getMarker,
+    };
+};
+
+// define players
+const player1 = Player('Player 1', 'O');
+const player2 = Player('Player 2', 'X');
+
+// init
+game.resetActivePlayer();
 displayController.updateGrid();
